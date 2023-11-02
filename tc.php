a973a629f0e9751ad48ead91284a28b7815ba2f1
Murat <85807376+yeniref@users.noreply.github.com>
2023-11-02 05:27:33 +0300

    Update tc.php

--- tc.php
+++ tc.php
@@ -10,5 +10,12 @@
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
 
+// SOAP Uyarısı
+function add_soap_warning() {
+    echo '<div class="notice notice-warning is-dismissible">
+        <p><strong>Önemli Bilgi:</strong> Bu eklenti SOAP protokolünü kullanarak Türk Kimlik Numarası (TCKN) doğrulaması yapmaktadır. SOAP protokolü, sunucunuzun SOAP istemcisini desteklemesini gerektirir. Lütfen sunucunuzun SOAP istemcisini etkinleştirdiğinizden ve SOAP istemcisini kullanabilecek yetkilere sahip olduğunuzdan emin olun.</p>
+    </div>';
+}
+add_action('admin_notices', 'add_soap_warning');
 
 require_once plugin_dir_path(__FILE__) . 'include/TcKontrol.php';
